9cdec4b5e308168b20a29fb6f1b12014c6bfc74b
Vladislava <vladislavayastremskaya@gmail.com>
2026-07-23 17:57:57 +0300

    second commit

--- src/app/create/create.ts
+++ src/app/create/create.ts
@@ -22,13 +22,31 @@ export class Create {
   classes = this.brawlerService.getClasses();
 
   isSubmitted = false;
+  tempImageString: string = 'images/brawlers/default-brawler.png';
 
   brawlerForm  = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(2)]),
     rarity: new FormControl('', [Validators.required]),
     class: new FormControl('', [Validators.required]),
-    // image: new FormControl(null, [Validators.required])
+    image: new FormControl<any>(null, [Validators.required])
   })
+
+  onFileSelected(event: Event): void {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.tempImageString = reader.result as string;
+
+        this.brawlerForm.patchValue({ image: this.tempImageString })
+    };
+
+      reader.readAsDataURL(file);
+  }
+}
 
   addValueToArray() {
     this.isSubmitted = true;
@@ -43,11 +61,13 @@ export class Create {
         name: formValue.name ?? '',
         rarity: formValue.rarity ?? '',
         class: formValue.class ?? '',
-        image: 'images/brawlers/default-brawler.png'
+        image: formValue.image ?? 'images/brawlers/default-brawler.png'
       }
 
       this.brawlers.push(newBrawler);
+
       this.brawlerForm.reset();
+      this.tempImageString = 'images/brawlers/default-brawler.png';
       this.isSubmitted = false;
     }
   }
