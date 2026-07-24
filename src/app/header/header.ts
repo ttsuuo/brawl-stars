107cc3e368701ec3813b7fdbeec3ee8f335743a1
Vladislava <vladislavayastremskaya@gmail.com>
2026-07-24 09:27:33 +0300

    fourth commit

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Navigation } from '../navigation/navigation';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [Navigation],
+  imports: [
+    Navigation,
+    RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
