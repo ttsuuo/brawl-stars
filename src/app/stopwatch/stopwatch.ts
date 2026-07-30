fd849f98509ccdebd767df11c71bd4ae42bd3d07
Vladislava <vladislavayastremskaya@gmail.com>
2026-07-30 17:50:04 +0300

    ninth commit

--- src/app/stopwatch/stopwatch.ts
+++ src/app/stopwatch/stopwatch.ts
@@ -1,25 +1,81 @@
-import { Component, inject } from '@angular/core';
-import { interval, Subscription, map } from 'rxjs';
-import { ChangeDetectorRef } from '@angular/core';
+import { Component, DestroyRef, ViewChild, ElementRef, inject, AfterViewInit } from '@angular/core';
+import { interval, Subscription, map, fromEvent, merge, Subject, Observable, timer, buffer, switchMap, BehaviorSubject, takeUntil, startWith, take, debounceTime, filter, scan } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-stopwatch',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stopwatch.html',
   styleUrl: './stopwatch.css',
 })
-export class Stopwatch {
+export class Stopwatch implements AfterViewInit {
   isRunning = false;
   count = '00:00:00';
   savedValue = 0;
 
-  timer?: Subscription;
-  private cdr = inject(ChangeDetectorRef);
-  private lastClickTime = 0;
+  tryTimer$ = interval(1000)
 
-  get buttonText(): string {
-    return this.isRunning ? 'STOP' : 'START';
+  private loadUserClick$ = new Subject<void>();
+  private stopUserClick$ = new Subject<void>();
+  savedTimer$ = new BehaviorSubject<number>(0);
+  lastSavedValue: number
+
+  timer$: Observable<number> = this.loadUserClick$.pipe(
+    switchMap(() => merge(
+      timer(0, 1000).pipe(
+        // map(() => {
+        //   const nextValue = this.savedTimer$.value + 1;
+        //   this.savedTimer$.next(nextValue);
+        //   return nextValue;
+        // }),
+        takeUntil(this.stopUserClick$)
+      ),
+      this.stopUserClick$.pipe(
+        map(() => 0),
+        take(1)
+      )
+    )), 
+    startWith(0)
+  )
+
+
+  startCount() {
+    this.loadUserClick$.next();
+    this.isRunning = true;
   }
+
+  stopCount() {
+    this.stopUserClick$.next();
+    this.isRunning = false;
+  }
+  
+  @ViewChild('waitBtn') 
+  waitButton!: ElementRef;
+  private destroyRef = inject(DestroyRef);
+
+  
+  waitBtnClicked() {
+    const click$ = fromEvent(this.waitButton.nativeElement, 'click');
+
+    const doubleClick$ = click$.pipe(
+      buffer(click$.pipe(debounceTime(300))),
+      map(clicks => clicks.length),
+      filter(clicksLength => clicksLength >= 2),
+      takeUntilDestroyed(this.destroyRef)
+    )
+
+    doubleClick$.subscribe(() => {
+      this.stopCount()
+    })
+  }
+  
+  ngAfterViewInit() {
+    this.waitBtnClicked()
+  }
+
+  timer?: Subscription;
+  private lastClickTime = 0;
 
   createIntervalFrom(savedValue: number) {
     return interval(1000).pipe(
@@ -43,7 +99,6 @@ export class Stopwatch {
     this.timer = this.createIntervalFrom(this.savedValue).subscribe((val) => {
       this.savedValue = val;
       this.count = this.formatTime(val);
-      this.cdr.detectChanges();
     })
     this.isRunning = true;
   }
@@ -70,18 +125,15 @@ export class Stopwatch {
   }
 
   resetTimer() {
-    this.timer?.unsubscribe();
-    this.savedValue = 0;
-    this.count = '00:00:00';
-    this.isRunning = false;
-    this.startTimer();
+    this.isRunning = true;
+    this.loadUserClick$.next();
   }
   
   buttonCLicked() {
     if (this.isRunning) {
-      this.stopTimer()
+      this.stopCount()
     } else {
-      this.startTimer()
+      this.startCount()
     }
   }
 
